e72f53db7746d4485d827736e47010bc1f433842
la790x <la790x@gmail.com>
2023-05-12 16:14:12 +0700

    rev code terminate.sh

--- shell/terminate.sh
+++ shell/terminate.sh
@@ -6,7 +6,7 @@ DB_PORT=3306
 DB_DATABASE=reverse_ssh_monitoring
 DB_USERNAME=root
 DB_PASSWORD=""
-DEVICE_ID=1
+DEVICE_ID="1"
 
 # Execute a SQL query using mysql client
 query="SELECT * FROM $TABLE_NAME WHERE id = $DEVICE_ID;"
