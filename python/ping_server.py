200d6c2c666782bcdc178d1c3ff42691d2022c17
la790x <la790x@gmail.com>
2023-05-15 17:52:32 +0700

    rev dikit

--- python/ping_server.py
+++ python/ping_server.py
@@ -1,12 +1,19 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+# Load the environment variables from .env file
+load_dotenv()
 
 def post_data_to_api(url, unique_code_device):
     try:
-        payload = {'unique_code_device': unique_code_device}
-        requests.post(url, data=payload)
+        payload = {
+            'unique_code_device': unique_code_device
+        }
+        requests.post(url, payload)
     except requests.exceptions.RequestException as e:
         print(f"Error occurred: {e}")
 
-api_url = "http://localhost:80/api/public/pings"
-unique_code_device = "v7cnp"
+api_url = os.environ.get("URL_SERVER_PING")
+unique_code_device = os.environ.get("UNIQUE_CODE_DEVICE")
 post_data_to_api(api_url, unique_code_device)
--- python/terminate_pid.py
+++ python/terminate_pid.py
@@ -11,18 +11,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # request terminate connection status
-terminated_connection_status = os.environ.get('TERMINATED_CONNECTION_STATUS')
-request_terminate_connection_status = os.environ.get('REQUEST_TERMINATE_CONNECTION_STATUS')
+terminated_connection_status = os.environ.get("TERMINATED_CONNECTION_STATUS")
+request_terminate_connection_status = os.environ.get(
+    "REQUEST_TERMINATE_CONNECTION_STATUS"
+)
 
 # Get the database connection details from environment variables
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_name = os.environ.get('DB_DATABASE')
-db_user = os.environ.get('DB_USERNAME')
-db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get("DB_HOST")
+db_port = os.environ.get("DB_PORT")
+db_name = os.environ.get("DB_DATABASE")
+db_user = os.environ.get("DB_USERNAME")
+db_password = os.environ.get("DB_PASSWORD")
 
 # Define the database connection URL
-db_url = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+db_url = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 # Create the SQLAlchemy engine
 engine = create_engine(db_url)
@@ -39,6 +41,7 @@ class ConnectionStatusModel(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String)
 
+
 # Define the rssh connection model
 class RSSHConnectionModel(Base):
     __tablename__ = "rssh_connections"
@@ -49,6 +52,7 @@ class RSSHConnectionModel(Base):
     connection_status_id = Column(BIGINT(unsigned=True))
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
 
 # Define the cron log model
 class CronLogModel(Base):
@@ -61,7 +65,9 @@ class CronLogModel(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
-    def __init__(self, file_name, log, is_error, rssh_connection_id, created_at, updated_at):
+    def __init__(
+        self, file_name, log, is_error, rssh_connection_id, created_at, updated_at
+    ):
         self.file_name = file_name
         self.log = log
         self.is_error = is_error
@@ -69,31 +75,34 @@ class CronLogModel(Base):
         self.created_at = created_at
         self.updated_at = updated_at
 
+
 # Connect to the database
 session = Session()
 
 # get time now
 def get_time_now():
     # Set the desired time zone
-    timezone = pytz.timezone('Asia/Jakarta')
+    timezone = pytz.timezone("Asia/Jakarta")
 
     # Get the current time in the specified time zone
     current_time = datetime.datetime.now(timezone)
 
     # Format the current time as a string
-    return current_time.strftime('%Y-%m-%d %H:%M:%S')
+    return current_time.strftime("%Y-%m-%d %H:%M:%S")
+
 
 # create data cron log
 def create_cron_log(session, log, is_error, rssh_connection_id):
     new_data = CronLogModel(
         file_name="terminate_pid.py",
-        log = log,
-        is_error = is_error,
-        rssh_connection_id = rssh_connection_id,
-        created_at = get_time_now(),
-        updated_at = get_time_now()
+        log=log,
+        is_error=is_error,
+        rssh_connection_id=rssh_connection_id,
+        created_at=get_time_now(),
+        updated_at=get_time_now(),
     )
     session.add(new_data)
+
 
 # function for terminate process linux by port
 def terminate_process_by_port(session, port, rssh_connection_id):
@@ -113,18 +122,28 @@ def terminate_process_by_port(session, port, rssh_connection_id):
         log = f"Error executing the lsof command : lsof -t -i : {port}"
         create_cron_log(session, log, "yes", rssh_connection_id)
 
+
 def update_status_rss_connection(session, rssh_connection_id, connection_status):
-    connection_status = session.query(ConnectionStatusModel).filter_by(name=connection_status).first()
-    session.query(RSSHConnectionModel).filter(RSSHConnectionModel.id == rssh_connection_id).update(
+    connection_status = (
+        session.query(ConnectionStatusModel).filter_by(name=connection_status).first()
+    )
+    session.query(RSSHConnectionModel).filter(
+        RSSHConnectionModel.id == rssh_connection_id
+    ).update(
         {
             RSSHConnectionModel.connection_status_id: connection_status.id,
-            RSSHConnectionModel.updated_at : get_time_now()
+            RSSHConnectionModel.updated_at: get_time_now(),
         },
-        synchronize_session=False
+        synchronize_session=False,
     )
 
+
 # Query the rss_connections table with the condition
-connection_status = session.query(ConnectionStatusModel).filter_by(name=request_terminate_connection_status).first()
+connection_status = (
+    session.query(ConnectionStatusModel)
+    .filter_by(name=request_terminate_connection_status)
+    .first()
+)
 rssh_connections = (
     session.query(RSSHConnectionModel)
     .filter_by(connection_status_id=connection_status.id)
