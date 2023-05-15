d1d99a4a4dd9780b7b4d4b891f1c51f2d21c2f76
la790x <la790x@gmail.com>
2023-05-15 22:10:14 +0700

    fix dikit

--- python/terminate_plink.py
+++ python/terminate_plink.py
@@ -1,11 +1,12 @@
+import os
 import sys
 import json
 import psutil
 import requests
+from dotenv import load_dotenv
 
-def post_data_to_api(url, unique_code_device, log, is_error):
-    payload = {'unique_code_device': unique_code_device, 'log': log, 'is_error': is_error}
-    requests.post(url, data=payload)
+# Load the environment variables from .env file
+load_dotenv()
 
 def is_valid_json(json_str):
     try:
@@ -14,45 +15,55 @@ def is_valid_json(json_str):
         return False
     return True
 
-def get_pid_app(app):
+def post_data_log_to_server(unique_code_device, log, is_error):
+    api_url = os.environ.get("URL_SERVER_RSSH_LOG")
+    payload = {
+        'unique_code_device': unique_code_device,
+        'log': log,
+        'is_error': is_error
+    }
+    requests.post(api_url, payload)
+
+def get_last_status_rssh_connection(unique_code_device):
+    try:
+        api_url = os.environ.get("URL_SERVER_CONNECTION_STATUS")
+        full_api_url = api_url + unique_code_device
+        response = requests.get(full_api_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return f"Error occurred: {e}"
+
+def get_pid_app_by_name(app):
     pidNumber = None
     for proc in psutil.process_iter(['pid', 'name']):
         if app == proc.info['name']:
             pidNumber = proc.info['pid']
     return pidNumber
 
-def get_connection_status(url, unique_code_device):
-    try:
-        full_url  = url + unique_code_device
-        response = requests.get(full_url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        message = f"Error occurred: {e}"
-        return message
-
-def kill_app_by_pid(pidNumber):
+def kill_app_by_pid(pidNumber, unique_code_device):
     try:
         process = psutil.Process(pidNumber)
         process.terminate()
-        print(f"PID {pidNumber} has been terminate")
+        message = f"PID {pidNumber} has been terminate"
+        post_data_log_to_server(unique_code_device, message, 'no')
     except psutil.NoSuchProcess as e:
         # send status to server
-        print(f"Error: {e}")
+        message = f"Error: {e}"
+        post_data_log_to_server(unique_code_device, message, 'yes')
 
-api_url = "http://localhost:80/api/public/rssh-connections/connection-status/"
-api_url_rssh_log = "http://localhost:80/api/public/rssh-logs"
-unique_code_device = "v7cnp"
-appName = "plink.exe"
+unique_code_device = os.environ.get("UNIQUE_CODE_DEVICE")
 
-# get connection status
-resultHitApi = get_connection_status(api_url, unique_code_device)
-json_string = json.dumps(resultHitApi)
+# get last connection status
+result_get_last_connection_status = get_last_status_rssh_connection(unique_code_device)
+json_string = json.dumps(result_get_last_connection_status)
+
+# check response data is json
 isValidJson = is_valid_json(json_string)
 
 if not isValidJson:
     message = f"Invalid json format"
-    post_data_to_api(api_url_rssh_log, unique_code_device, message, 'yes')
+    post_data_log_to_server(unique_code_device, message, 'yes')
     sys.exit(0)
 
 # Parse the JSON data
@@ -61,7 +72,7 @@ http_code = data["http_code"]
 
 if 200 != http_code:
     message = f"Result http is {http_code}"
-    post_data_to_api(api_url_rssh_log, unique_code_device, message, 'yes')
+    post_data_log_to_server(unique_code_device, message, 'yes')
     sys.exit(0)
 
 status_terminate_plink_exe = False
@@ -74,17 +85,19 @@ if connection_status_rssh == "request terminate":
     status_terminate_plink_exe = True
 
 if not status_terminate_plink_exe:
-    print(f"Done")
+    message = "no connection status required for restart"
+    post_data_log_to_server(unique_code_device, message, 'no')
     sys.exit(0)
 
-pidNumber = get_pid_app(appName)
+appName = os.environ.get("PLINK_EXE")
+pidNumber = get_pid_app_by_name(appName)
+
 if pidNumber is None:
-    # send status to server
     message = f"PID {appName} not found"
-    post_data_to_api(api_url_rssh_log, unique_code_device, message, 'yes')
-    sys.exit()
+    post_data_log_to_server(unique_code_device, message, 'yes')
+    sys.exit(0)
 
 # kill pid & send status to server
-kill_app_by_pid(pidNumber)
+kill_app_by_pid(pidNumber, unique_code_device)
 message = f"Success kill plink.exe"
-post_data_to_api(api_url_rssh_log, unique_code_device, message, 'no')
+post_data_log_to_server(unique_code_device, message, 'no')
