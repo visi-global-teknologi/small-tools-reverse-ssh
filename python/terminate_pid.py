a4dee007b76e87e8adcb422d1135069d6456ec0e
la790x <la790x@gmail.com>
2023-05-15 15:08:11 +0700

    debug

--- python/terminate_pid.py
+++ python/terminate_pid.py
@@ -118,6 +118,7 @@ if rssh_connections:
     for rsshc in rssh_connections:
         id = rsshc.id
         local_port = rsshc.local_port
+        print (local_port);
         terminate_process_by_port(session, local_port, id)
 else:
     print("No request to dismiss PID")
