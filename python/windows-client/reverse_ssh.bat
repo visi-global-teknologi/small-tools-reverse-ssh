99e0dd4549b4a3eb89a1ee9bbb0b45f03d44d9cd
la790x <la790x@gmail.com>
2023-07-20 22:22:18 +0700

    fix dikit

--- python/windows-client/reverse_ssh.bat
+++ python/windows-client/reverse_ssh.bat
@@ -5,7 +5,7 @@ set SERVER_PORT=4001
 set SERVER_PASSWORD=H41kalGantengSekali
 set LOCAL_PORT=3389
 set LOCAL_HOST=localhost
-set "PLINK_PATH=C:Users\Admin\Documents\small-tools-reverse-ssh\python\windows-client\plink.exe"
+set "PLINK_PATH=C:\Users\Admin\Documents\small-tools-reverse-ssh\python\windows-client\plink.exe"
 
 echo Starting reverse ssh connection....
 echo %SERVER_PASSWORD% y | "%PLINK_PATH%" -ssh -pw %SERVER_PASSWORD% -N -R %SERVER_PORT%:%LOCAL_HOST%:%LOCAL_PORT% %SERVER_USERNAME%@%SERVER_IP%
